062026547ff5aa76341908f422981aaf3bed4fcf
Curtis Sand <curtis@curtissand.com>
2015-04-08 21:16:07 -0600

    Fixed up lib.model.coord and added unittests
    The system property had a small bug in it's calculation. Fixed things up
    for pep8 and pyflakes. SystemCoord had a few issues that required a base
    class to get the desired behaviour.

--- lib/model/coord.py
+++ lib/model/coord.py
@@ -1,52 +1,60 @@
 __all__ = ['Coord', 'SystemCoord']
 
 
-class Coord(object):
-    def __init__(self, x=None, y=None, planet=None):
-        if x is None: x = 0
-        if y is None: y = 0
-        if planet is None: planet = 0
-        self.x = float(x)
-        self.y = float(y)
-        self.planet = int(planet)
+class BaseCoord(object):
+    def __init__(self, x=None, y=None):
+        if x is None:
+            x = 0
+        if y is None:
+            y = 0
+        self.x = str(float(x))
+        self.y = str(float(y))
 
     @property
     def sector(self):
-        return (int(str(self.x).split('.')[0]),
-                int(str(self.y).split('.')[0]))
+        return (int(self.x.split('.')[0]),
+                int(self.y.split('.')[0]))
 
     @sector.setter
     def sector(self, value):
         sec_x = str(int(value[0]))
         sec_y = str(int(value[1]))
-        self.x = float('.'.join([sec_x, str(self.x).split('.')[1]]))
-        self.y = float('.'.join([sec_y, str(self.y).split('.')[1]]))
+        self.x = '.'.join([sec_x, str(self.x).split('.')[1]])
+        self.y = '.'.join([sec_y, str(self.y).split('.')[1]])
 
     @property
     def system(self):
-        return (int(str(self.x).split('.')[1]),
-                int(str(self.y).split('.')[1]))
+        return (int(self.x.split('.')[1]),
+                int(self.y.split('.')[1]))
 
     @system.setter
     def system(self, value):
         sys_x = str(int(value[0]))
         sys_y = str(int(value[1]))
-        self.x = float('.'.join([sys_x, str(self.x).split('.')[0]]))
-        self.y = float('.'.join([sys_y, str(self.y).split('.')[0]]))
+        self.x = '.'.join([str(self.x).split('.')[0], sys_x])
+        self.y = '.'.join([str(self.y).split('.')[0], sys_y])
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class Coord(BaseCoord):
+    def __init__(self, x=None, y=None, planet=None):
+        if planet is None:
+            planet = 0
+        self.planet = str(int(planet))
+        super().__init__(x, y)
 
     def __eq__(self, other):
         return (other.x == self.x and
                 other.y == self.y and
                 other.planet == self.planet)
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
     def __hash__(self):
         return hash((self.x, self.y, self.planet))
 
     def __repr__(self):
-        return "Coord(%s, %s, %s)" % (self.x, self.y, self.planet)
+        return "Coord({}, {}, {})".format(self.x, self.y, self.planet)
 
     def __str__(self):
         return str((self.x, self.y, self.planet))
@@ -55,10 +63,29 @@ class Coord(object):
         return (self.x, self.y, self.planet)
 
     def __setstate__(self, state):
-        (self.x, self.y, self.planet) = state
+        self.x, self.y, self.planet = state
 
 
 class SystemCoord(Coord):
     def __init__(self, x=None, y=None):
-        super(SystemCoord, self).__init__(x, y, None)
-        self.planet = None
+        super().__init__(x, y, None)
+        del(self.planet)
+
+    def __eq__(self, other):
+        return (other.x == self.x and
+                other.y == self.y)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __repr__(self):
+        return "SystemCoord({}, {})".format(self.x, self.y)
+
+    def __str__(self):
+        return str((self.x, self.y))
+
+    def __getstate__(self):
+        return (self.x, self.y)
+
+    def __setstate__(self, state):
+        self.x, self.y = state
---
+++ tests/lib/model/test_coord.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+import unittest
+import random
+import re
+
+from lib.model import coord
+
+
+class BaseCoordTest(unittest.TestCase):
+    def setUp(self):
+        self.coord = coord.Coord(0, 0, 0)
+
+    def test_sector(self):
+        self._property_test('sector')
+
+    def test_system(self):
+        self._property_test('system')
+
+    def _property_test(self, name):
+        if name == 'planet':
+            test_value = self.get_test_values(planet=True)
+        else:
+            test_value = self.get_test_values(planet=False)
+        setattr(self.coord, name, test_value)
+        self.assertEqual(getattr(self.coord, name), test_value)
+
+    def get_test_values(self, planet=False):
+        count = 2
+        if planet:
+            count = 1
+        return tuple([random.randint(0, 1000) for _ in range(count)])
+
+
+class TestCoord(BaseCoordTest):
+    def test_planet(self):
+        self._property_test('planet')
+
+    def test_hash(self):
+        start_hash = hash(self.coord)
+        test_sector = self.get_test_values()
+        test_system = self.get_test_values()
+        test_planet = self.get_test_values(planet=True)
+        example_coord = coord.Coord()
+        self.assertEqual(hash(example_coord), start_hash)
+        example_coord.sector = test_sector
+        example_coord.system = test_system
+        example_coord.planet = test_planet
+        self.coord.sector = test_sector
+        self.coord.system = test_system
+        self.coord.planet = test_planet
+        self.assertEqual(hash(self.coord), hash(example_coord))
+
+    def test_repr(self):
+        expected = 'Coord\(\d\.\d, \d\.\d, \d\)'
+        self.assertTrue(re.search(expected, repr(self.coord)))
+
+    def test_str(self):
+        expected = '\(\'\d\.\d\', \'\d\.\d\', \'\d\'\)'
+        self.assertTrue(re.search(expected, str(self.coord)))
+
+    def test_getstate(self):
+        expected = ('0.0', '0.0', '0')
+        self.assertEqual(expected, self.coord.__getstate__())
+
+    def test_setstate(self):
+        self.coord.sector = self.get_test_values()
+        self.coord.system = self.get_test_values()
+        self.coord.planet = self.get_test_values(planet=True)
+        test_obj = coord.Coord()
+        test_obj.__setstate__((self.coord.x, self.coord.y, self.coord.planet))
+        self.assertEqual((self.coord.x, self.coord.y, self.coord.planet),
+                         (test_obj.x, test_obj.y, test_obj.planet))
+
+
+class TestSystemCoord(BaseCoordTest):
+    def setUp(self):
+        self.coord = coord.SystemCoord(0, 0)
+
+    def test_no_planet(self):
+        self.assertFalse(hasattr(self.coord, 'planet'))
+
+    def test_hash(self):
+        start_hash = hash(self.coord)
+        test_sector = self.get_test_values()
+        test_system = self.get_test_values()
+        example_coord = coord.SystemCoord()
+        self.assertEqual(hash(example_coord), start_hash)
+        example_coord.sector = test_sector
+        example_coord.system = test_system
+        self.coord.sector = test_sector
+        self.coord.system = test_system
+        self.assertEqual(hash(self.coord), hash(example_coord))
+
+    def test_getstate(self):
+        expected = ('0.0', '0.0')
+        self.assertEqual(expected, self.coord.__getstate__())
+
+    def test_repr(self):
+        expected = 'SystemCoord\(\d\.\d, \d\.\d\)'
+        self.assertTrue(re.search(expected, repr(self.coord)))
+
+    def test_str(self):
+        expected = '\(\'\d\.\d\', \'\d\.\d\'\)'
+        self.assertTrue(re.search(expected, str(self.coord)))
+
+    def test_setstate(self):
+        self.coord.sector = self.get_test_values()
+        self.coord.system = self.get_test_values()
+        test_obj = coord.SystemCoord()
+        test_obj.__setstate__((self.coord.x, self.coord.y))
+        self.assertEqual((self.coord.x, self.coord.y),
+                         (test_obj.x, test_obj.y))
+
+
+if __name__ == "__main__":
+    unittest.main()
