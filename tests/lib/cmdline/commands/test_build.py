ce8ea142b7e35e71ce239556aa6837364d35d9f0
Curtis Sand <curtis@curtissand.com>
2015-09-11 11:22:08 -0600

    cmdline: implement unit tests for build command

--- tests/lib/cmdline/commands/test_build.py
+++ tests/lib/cmdline/commands/test_build.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest import skip
+from unittest.mock import Mock
 
+import lib.model as model
 import lib.cmdline.commands as commands
 
 from .test_base import BaseCommandTest
@@ -24,7 +25,32 @@ class BuildTest(BaseCommandTest):
         super().setUp()
         self.command_class = commands.Build
         self.alias_commands = []
+        self.mock_planet = Mock()
+        mock_building = Mock()
+        mock_building.name = 'Mine'
+        self.mock_planet.get_available_buildings.return_value = [
+            (mock_building, 1)]
+        self.mock_planet.build.return_value = True
 
-    @skip('NI')
+    def test_build_no_current_planet(self):
+        build = self.get_instance()
+        build.current_object = None
+        build.do_build('')
+        self.assertEqual(build.current_object, None)
+
+    def get_instance_with_current_object(self):
+        build = self.get_instance()
+        build.current_object = (model.Coord(), self.mock_planet)
+        return build
+
+    def test_build_no_specified_type(self):
+        build = self.get_instance_with_current_object()
+        build.do_build('')
+        self.assertTrue(self.mock_planet.get_available_buildings.called)
+        self.assertFalse(self.mock_planet.build.called)
+
     def test_build(self):
-        pass
+        build = self.get_instance_with_current_object()
+        build.do_build('Mine')
+        self.assertFalse(self.mock_planet.get_available_buildings.called)
+        self.assertTrue(self.mock_planet.build.called)
