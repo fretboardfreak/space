d6af105bf4c7c06bf29b591c3d56c8550b0e0a0b
Curtis Sand <curtis@curtissand.com>
2015-09-12 12:51:20 -0600

    cmdline: refactor build tests to use base classes current object
    The base test for commands now defines a current object so individual
    command tests don't need to implement their own.

--- tests/lib/cmdline/commands/test_build.py
+++ tests/lib/cmdline/commands/test_build.py
@@ -14,7 +14,6 @@
 
 from unittest.mock import Mock
 
-import lib.model as model
 import lib.cmdline.commands as commands
 
 from .test_base import BaseCommandTest
@@ -24,13 +23,7 @@ class BuildTest(BaseCommandTest):
     def setUp(self):
         super().setUp()
         self.command_class = commands.Build
-        self.alias_commands = []
-        self.mock_planet = Mock()
-        mock_building = Mock()
-        mock_building.name = 'Mine'
-        self.mock_planet.get_available_buildings.return_value = [
-            (mock_building, 1)]
-        self.mock_planet.build.return_value = True
+        self.mock_planet.get_available_buildings = Mock(return_value=[])
 
     def test_build_no_current_planet(self):
         build = self.get_instance()
@@ -38,19 +31,16 @@ class BuildTest(BaseCommandTest):
         build.do_build('')
         self.assertEqual(build.current_object, None)
 
-    def get_instance_with_current_object(self):
-        build = self.get_instance()
-        build.current_object = (model.Coord(), self.mock_planet)
-        return build
-
     def test_build_no_specified_type(self):
-        build = self.get_instance_with_current_object()
+        self.mock_planet.build = Mock()
+        build = self.get_instance()
         build.do_build('')
         self.assertTrue(self.mock_planet.get_available_buildings.called)
         self.assertFalse(self.mock_planet.build.called)
 
     def test_build(self):
-        build = self.get_instance_with_current_object()
+        self.mock_planet.build = Mock()
+        build = self.get_instance()
         build.do_build('Mine')
         self.assertFalse(self.mock_planet.get_available_buildings.called)
         self.assertTrue(self.mock_planet.build.called)
