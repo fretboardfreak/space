6f434658c1a575e67a2916055e84808c82379568
Curtis Sand <curtis@curtissand.com>
2015-10-05 15:39:37 -0600

    planet: trigger delayed actions when planet.update is called

--- lib/model/planet.py
+++ lib/model/planet.py
@@ -19,7 +19,7 @@ from logging import debug
 from lib.error import ModelObjectError
 from lib.namegen import NameGen
 
-from .update import ResourceUpdater
+from .update import ResourceUpdater, delayed_action_trigger
 from .resources import Resources
 from .building import ALL_BUILDINGS, get_building
 
@@ -95,6 +95,7 @@ class Planet(object):
             rates += bld.modifier
         return rates
 
+    @delayed_action_trigger
     def update(self):
         updater = ResourceUpdater(self.last_update, self.resources,
                                   self.rates, self.max_resources)
